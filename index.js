c1b6488df25f18c1b087eff1f658504bed155ca0
Marwa Mohammed <52058731+MarwaElasuty@users.noreply.github.com>
2024-02-14 15:50:25 +0200

    cvse

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-        var toggle = document.querySelector("#nav-links");
-        console.log(toggle);
-
-       function appear(){
-        console.log(toggle);
-        console.log(toggle.style);
-
-        console.log(toggle.style.display);
-
-        if(toggle.style.display=="block"){
-          toggle.style.display="none";
-        }
-        else{
-          toggle.style.display="block";
-        }
-       }
- 
+function appear() {
+  document.querySelector(".ul-list").classList.toggle("display");
+ }
